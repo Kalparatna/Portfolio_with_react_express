b2c6f80ffbedf9152312b689896cbd1d01353c11
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-01-23 11:50:50 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,9 +11,14 @@ const MONGO_URI = 'mongodb+srv://Kp:admin%40123@cluster1.2xten.mongodb.net/conta
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+const corsOptions = {
+  origin: 'https://portfolio1-git-main-kalparatnas-projects.vercel.app', // Your frontend URL
+  methods: 'GET,POST',
+};
+app.use(cors(corsOptions));
 
 // Connect to MongoDB (without deprecated options)
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
