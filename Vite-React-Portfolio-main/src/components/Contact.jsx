8c5afd68f9254b3c9b2e069f7f04de11cb24452a
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-01-23 11:42:54 +0530

    Update Contact.jsx

--- Vite-React-Portfolio-main/src/components/Contact.jsx
+++ Vite-React-Portfolio-main/src/components/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portfoliobackend-gray.vercel.app/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
