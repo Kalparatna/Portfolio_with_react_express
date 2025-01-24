8df70193c3f6fc7443231494dd60213fc34393bb
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-01-24 16:49:55 +0530

    Update About.jsx

--- Vite-React-Portfolio-main/src/components/About.jsx
+++ Vite-React-Portfolio-main/src/components/About.jsx
@@ -3,21 +3,23 @@ import '../styles/About.css';
 
 const About = () => {
   return (
-    <section id="about" className="container about-section">
-      <div className="row">
-        {/* Left Section: Image */}
-        <div className="col-md-4 text-center">
-          <img src="./1.png" alt="About Me" className="about-img" />
-        </div>
+    <section id="about" className="about-section">
+      <div className="container">
+        <div className="row">
+          {/* Image Section */}
+          <div className="col-md-4 text-center">
+            <img src="./1.png" alt="About Me" className="about-img" />
+          </div>
 
-        {/* Right Section: About Text */}
-        <div className="col-md-8">
-          <h2 className="about-title">About Me</h2>
-          <p className="about-description">
-            I am currently pursuing my B.Tech in Information Technology at SVKM IOT, Dhule. I have already completed my Diploma in
-            Mechanical Engineering at Government Polytechnic, Jalgaon. Currently looking for opportunities in Software Industries to
-            direct my career towards growth and success.
-          </p>
+          {/* Text Section */}
+          <div className="col-md-8">
+            <h2 className="about-title">About Me</h2>
+            <p className="about-description">
+              I am currently pursuing my B.Tech in Information Technology at SVKM IOT, Dhule. I have already completed my Diploma in
+              Mechanical Engineering at Government Polytechnic, Jalgaon. Currently looking for opportunities in Software Industries to
+              direct my career towards growth and success.
+            </p>
+          </div>
         </div>
       </div>
     </section>
